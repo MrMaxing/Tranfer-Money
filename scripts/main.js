af1703b793c313f24a1eeb9f04edcf148f1203db
Mr Maxing <112796660+MrMaxing@users.noreply.github.com>
2023-05-07 17:33:18 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,12 @@ import * as fm from '@minecraft/server-ui';
 import { getScore, doMove } from './server/library.api';
 // Objective to transfer money
 const objective = 'money';
+const prefix = '!'
+const key = 'pay'
 mc.world.events.beforeChat.subscribe((events) => {
     const player = events.sender;
     const message = events.message;
-    if (!message.startsWith('!tranfer'))
+    if (!message.startsWith(`${prefix}${key}`))
         return;
     events.cancel = true;
     doMove(player, () => tranfer(player));
